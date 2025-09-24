6e05bfee141ba403b5e6e7a34cfaffe7a4418898
Fubowen <2251754654@qq.com>
2025-09-24 16:34:42 +0800

    更新线程模块，添加对 utils/a.js 和 utils/b.js 的导出

--- src/thread/thread.module.js
+++ src/thread/thread.module.js
@@ -1,1 +1,3 @@
-export * as hashCode from '../modules/hashCode.js'
+export * as hashCode from "../modules/hashCode.js"
+export * as a from "file:///D:/utils/@fubowen/test/src/utils/a.js"
+export * as b from "file:///D:/utils/@fubowen/test/src/utils/b.js"
